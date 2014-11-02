2718cd58d52ee2e9043a65d023c448294ace9bae
André Freitas <p.andrefreitas@gmail.com>
2014-11-02 19:41:31 +0000

    O jogo começa com um terreno. Quando o terreno é tracked começa o gameScript.

---
+++ Assets/Scripts/gameScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gameScript : MonoBehaviour {
+	string state = "waiting";
+
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void startGame(){
+		state = "started";
+		Debug.Log ("Game Started");
+
+	}
+}
---
+++ Assets/Scripts/terrainScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class terrainScript : MonoBehaviour {
+
+	gameScript game;
+	GameObject gameScriptObject;
+
+
+	void Start () {
+		// Get the GameScript reference
+		gameScriptObject = GameObject.Find("GameScript");
+		game = (gameScript) gameScriptObject.GetComponent(typeof(gameScript));
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	
+	}
+
+	void OnBecameVisible() {
+		game.startGame ();
+	}
+}
