5aa63bce78cc8701cf124f4b116df1448d48cde4
André Freitas <p.andrefreitas@gmail.com>
2014-11-04 19:22:50 +0000

    Colocados elementos da GUI

--- Assets/Scripts/GUIScript.cs
+++ Assets/Scripts/GUIScript.cs
@@ -3,10 +3,44 @@ using System.Collections;
 
 public class GUIScript : MonoBehaviour {
 
+	GUIStyle messagesStyle;
+	GUIStyle scoreStyle;
+	public Texture2D messageTexture;
+	public Texture2D scoreTexture;
+	
+
 	// Use this for initialization
 	void Start () {
+
+		initStyles ();
+
+
+
 	
 	}
+
+	void initStyles(){
+		messagesStyle = new GUIStyle ();
+		messagesStyle.fontSize = 80;
+		messagesStyle.richText = true;
+		messagesStyle.normal.textColor = Color.white;
+		messagesStyle.normal.background = messageTexture;
+		messagesStyle.padding = new RectOffset(300,0, 55, 20);
+		messagesStyle.alignment = TextAnchor.UpperLeft;
+
+		scoreStyle = new GUIStyle ();
+		scoreStyle.fontSize = 80;
+		scoreStyle.richText = true;
+		scoreStyle.normal.textColor = Color.white;
+		scoreStyle.normal.background = scoreTexture;
+		scoreStyle.padding = new RectOffset(0,0, 55, 20);
+		scoreStyle.alignment = TextAnchor.UpperCenter;
+
+
+
+
+	}
+
 	
 	// Update is called once per frame
 	void Update () {
@@ -14,6 +48,14 @@ public class GUIScript : MonoBehaviour {
 	}
 
 	void OnGUI () {
-		GUI.Label (new Rect (25, 25, 100, 30), "Label");
+
+	}
+
+	void displayScore(int score){
+		GUI.Label(new Rect(50, 50, 698, 200), score.ToString() + " pontos", scoreStyle);
+	}
+
+	void displayMessage(string message){
+		GUI.Label(new Rect(100, 100, 1397, 202), message, messagesStyle);
 	}
 }
